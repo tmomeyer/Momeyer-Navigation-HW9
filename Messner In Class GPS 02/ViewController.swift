e1023babd83b470b624b756ee5cd762c642849c3
Brad D Messner <bmessner@setonhill.edu>
2020-03-23 09:48:27 -0400

    Initial Commit

---
+++ Messner In Class GPS 02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messner In Class GPS 02
+//
+//  Created by Brad D. Messner on 3/23/20.
+//  Copyright © 2020 Brad D. Messner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Messner In Class GPS 02Tests/Messner_In_Class_GPS_02Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Messner_In_Class_GPS_02Tests.swift
+//  Messner In Class GPS 02Tests
+//
+//  Created by Brad D. Messner on 3/23/20.
+//  Copyright © 2020 Brad D. Messner. All rights reserved.
+//
+
+import XCTest
+@testable import Messner_In_Class_GPS_02
+
+class Messner_In_Class_GPS_02Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
